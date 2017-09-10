6b4b07f6804b2e2f1bdf30e5d2e8583f38e679be
sun-huihui <502477576@qq.com>
2017-09-10 20:10:41 +0800

    模型

--- ttsx_xm/User/models.py
+++ ttsx_xm/User/models.py
@@ -7,3 +7,10 @@ class UserInfo(models.Model):
     userEmail = models.CharField(max_length=30)
     isValid = models.BooleanField(default=True)
     isActive = models.BooleanField(default=False)
+
+
+class UserAddressInfo(models.Model):
+    uname = models.CharField(max_length=20)
+    uaddress = models.CharField(max_length=100)
+    uphone = models.CharField(max_length=11)
+    user = models.ForeignKey('UserInfo')
