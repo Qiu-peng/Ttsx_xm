c8786e4e55399184bfa05decfd9e0e689d2b6fd9
sun-huihui <502477576@qq.com>
2017-09-10 20:15:49 +0800

    模型

--- ttsx_xm/User/models.py
+++ ttsx_xm/User/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class UserInfo(models.Model):
     userName = models.CharField(max_length=20)
@@ -7,3 +8,10 @@ class UserInfo(models.Model):
     userEmail = models.CharField(max_length=30)
     isValid = models.BooleanField(default=True)
     isActive = models.BooleanField(default=False)
+
+
+class UserAddressInfo(models.Model):
+    uName = models.CharField(max_length=20)
+    uAddress = models.CharField(max_length=100)
+    uPhone = models.CharField(max_length=11)
+    user = models.ForeignKey('UserInfo')
