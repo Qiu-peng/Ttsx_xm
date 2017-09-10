cf86cfef17317a4d7a96769337f1d591c81b7f07
sun-huihui <502477576@qq.com>
2017-09-10 20:33:28 +0800

    管理类

--- ttsx_xm/User/models.py
+++ ttsx_xm/User/models.py
@@ -1,7 +1,27 @@
 from django.db import models
 
 
-# Create your models here.
+class UserInfoManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(isValid=True)
+
+    def create(self, name, psw, email):
+        user = UserInfo()
+        user.userName = name
+        user.userPsw = psw
+        user.userEmail = email
+        return user
+
+
+class AddressInfoManager(models.Manager):
+    def create(self, adrName, adrPsw, adrEmail):
+        adress = UserAddressInfo()
+        adress.userName = adrName
+        adress.userPsw = adrPsw
+        adress.userEmail = adrEmail
+        return adress
+
+
 class UserInfo(models.Model):
     userName = models.CharField(max_length=20)
     userPsw = models.CharField(max_length=20)
@@ -15,4 +35,3 @@ class UserAddressInfo(models.Model):
     uAddress = models.CharField(max_length=100)
     uPhone = models.CharField(max_length=11)
     user = models.ForeignKey('UserInfo')
-    
