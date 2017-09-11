3347b108becb86fb02ca8855512fbb33acfb51d8
YXB <616353084@qq.com>
2017-09-11 19:37:44 +0800

    改动了index.html部分动态获取

--- ttsx_xm/Goods/views.py
+++ ttsx_xm/Goods/views.py
@@ -1,10 +1,35 @@
 from django.shortcuts import render
+from .models import *
 
 
 # Create your views here.
 # 首页视图
 def index(request):
-    return render(request, 'Goods/index.html')
-
-
-
+    """按新添加推荐商品3个，按点击量过滤展示商品图片4张"""
+    # 获取新添加的三个水果，和点击量最高的四个水果
+    fruit = GoodsInfo.objects.filter(gtype=1).order_by("-id")[:3]
+    fruit2 = GoodsInfo.objects.filter(gtype=1).order_by('-gclick')[:4]
+    # 获取新添加的三个海鲜水产，和点击量最高的四个海鲜水产
+    fish = GoodsInfo.objects.filter(gtype=2).order_by("-id")[:3]
+    fish2 = GoodsInfo.objects.filter(gtype=2).order_by('-gclick')[:4]
+    # 获取新添加的三个肉类，和点击量 最高的四个肉类
+    meal = GoodsInfo.objects.filter(gtype=3).order_by("-id")[:3]
+    meal2 = GoodsInfo.objects.filter(gtype=3).order_by('-gclick')[:4]
+    # 获取新添加的三个禽类蛋品，和点击量最高的四个禽类蛋品
+    eggs = GoodsInfo.objects.filter(gtype=4).order_by("-id")[:3]
+    eggs2 = GoodsInfo.objects.filter(gtype=4).order_by('-gclick')[:4]
+    # 获取新添加的三个蔬菜，和点击量最高的四个蔬菜
+    vege = GoodsInfo.objects.filter(gtype=5).order_by("-id")[:3]
+    vege2 = GoodsInfo.objects.filter(gtype=5).order_by('-gclick')[:4]
+    # 获取新添加的三个速冻食品，和点击量最高的四个速冻食品
+    fastFrozen = GoodsInfo.objects.filter(gtype=6).order_by("-id")[:3]
+    fastFrozen2 = GoodsInfo.objects.filter(gtype=6).order_by('-gclick')[:4]
+    context = {
+        'fruit': fruit, 'fruit2': fruit2,
+        'fish': fish, 'fish2': fish2,
+        'meal': meal, 'meal2': meal2,
+        'eggs': eggs, 'eggs2': eggs2,
+        'vege': vege, 'vege2': vege2,
+        'fastFrozen': fastFrozen, 'fastFrozen2': fastFrozen2
+    }
+    return render(request, 'Goods/index.html', context)
