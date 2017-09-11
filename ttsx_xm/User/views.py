0d36119298bc4eb60bee9ba466e1b660d8119fe5
sun-huihui <502477576@qq.com>
2017-09-11 21:49:36 +0800

    同步login

--- ttsx_xm/User/views.py
+++ ttsx_xm/User/views.py
@@ -10,20 +10,30 @@ def login(request):
 
 def toLogin(request):
     uname = request.POST.get('name')
-    the_user = UserInfo.users.get(userName=uname)
-    upwd = the_user.userPsw
-    return JsonResponse({'pwd':upwd})
+    the_user = UserInfo.users.filter(userName=uname)
+    print(the_user.exists())
+    # aaa = the_user.exists()
+    if the_user.exists():
+        upwd = the_user.userPsw
+        print(upwd)
+        return JsonResponse({'pwd': upwd})
+    else:
+        print('ccc')
+        return JsonResponse({'sss': 'nonono'})
+
 
 def toindex(request):
     uname = request.GET.get('name')
-    context={'uname':uname}
-    # print(context)
+    context={'uname': uname}
+    print(context)
     return render(request, 'Goods/index.html', context)
     # return redirect('Goods/index.html')
+
 
 # 显示注册页面
 def register(request):
     return render(request, 'User/register.html')
+
 
 # 处理用户填入的注册信息
 def regist(request):
