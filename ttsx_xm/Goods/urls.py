3219d5ca884c25168592a60bf53866bdc1dd7996
YXB <616353084@qq.com>
2017-09-11 10:49:30 +0800

    修改了部分html跳转

--- ttsx_xm/Goods/urls.py
+++ ttsx_xm/Goods/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index)
+    url(r'^$', views.index),  # 调用首页视图
 
 ]
 
--- ttsx_xm/Goods/views.py
+++ ttsx_xm/Goods/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 
 
 # Create your views here.
+# 首页视图
 def index(request):
     return render(request, 'Goods/index.html')
+
+
