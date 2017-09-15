46535d51674bfc3f09dd4175d70c6fda1b5bbc52
leixiaohui <77434590@qq.com>
2017-09-15 22:04:16 +0800

    倒计时

--- ttsx_xm/static/js/submit.js
+++ ttsx_xm/static/js/submit.js
@@ -7,43 +7,56 @@
 $(function () {
     // 获取状态码 以确定最终要跳转的页面
     var statuscode = $('#status').val();
-    if(statuscode == '1') {
-        $('.popup_con').fadeIn('fast', function () {
+    var timeleft = 5;
 
-            setTimeout(function () {
-                $('.popup_con').fadeOut('fast', function () {
-                    // 回到首页
-                    window.location.href = '/';
-                });
-            }, 3000)
-
-
-        });
+    function timing(path) {
+        //改变显示的时间值
+        $('#count').html(timeleft);
+        //时间值减1
+        timeleft--;
+        //当时间值为0时，跳转
+        if (timeleft < 1) {
+            window.location.href = path;
         }
-    else if(statuscode == '2'){
+    }
 
-        $('.popup p').html('超过库存量请修改');
+    if (statuscode == '1') {
+        $('.popup_con').show(function () {
 
-        $('.popup_con').fadeIn('fast', function () {
+            timing('/');
+            // 每一秒执行一次函数
+            setInterval(function () {
+                timing('/');
+            }, 1000);
 
-            setTimeout(function () {
-                $('.popup_con').fadeOut('fast', function () {
-                    // 回到首页
-                    window.location.href = '/Cart/cart/';
-                });
-            }, 3000)
         });
     }
-    else {
-         $('.popup p').html('超过库存量请修改');
-        $('.popup_con').fadeIn('fast', function () {
+    // 回到立即购买页面
+    else if (statuscode == '2') {
 
-            setTimeout(function () {
-                $('.popup_con').fadeOut('fast', function () {
-                    // 回到首页
-                    window.location.href = '/Cart/cart/';
-                });
-            }, 3000)
+        $('.popup p').html('超过库存量请修改!');
+
+        $('.popup_con').show(function () {
+
+            timing('/Cart/cart/');
+            // 每一秒执行一次函数
+            setInterval(function () {
+                timing('/Cart/cart/');
+            }, 1000);
+
+
+        });
+    }
+    // 回到购物车
+    else {
+        $('.popup p').html('超过库存量请修改!');
+        $('.popup_con').show(function () {
+
+            timing('/Cart/cart/');
+            // 每一秒执行一次函数
+            setInterval(function () {
+                timing('/Cart/cart/');
+            }, 1000);
         });
     }
 });
