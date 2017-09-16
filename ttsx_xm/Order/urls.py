774cc60662b59a14f5da178d193b33cb1b1eedd6
leixiaohui <77434590@qq.com>
2017-09-16 21:04:26 +0800

    订单更新

--- ttsx_xm/Order/urls.py
+++ ttsx_xm/Order/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
 
     url(r'^$',views.showorder),
     url(r'^handle_order/$',views.handle_order),
+    url(r'^code/$',views.code),
     url(r'^delete/$', views.delete),  # 退出登录,删除cookie
 
 ]
